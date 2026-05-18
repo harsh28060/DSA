6ee8b1c0362e833275255e08be10ab910165df1b
harsh28060 <harshpanchigar@gmail.com>
2026-05-19 00:44:29 +0530

    first medium

---
+++ Medium/Q347.py
@@ -0,0 +1,24 @@
+def topKFrequent(nums, k):
+    count = {}
+    freq = [[] for i in range(len(nums)+1)]
+
+    # Count frequency
+    for i in nums:
+        count[i] = 1 + count.get(i,0)
+
+    # Put into buckets
+    for n, c in count.items():
+        freq[c].append(n)
+
+    # Get top k
+    res = []
+    for i in range(len(freq)-1,0,-1):
+        for n in freq[i]:
+            res.append(n)
+            if len(res) == k:
+                return res
+
+nums = [1]
+k = 1
+
+print(topKFrequent(nums, k))
---
+++ test.py
@@ -0,0 +1,20 @@
+def topKFrequent(nums , k):
+    count = {}
+    freq = [[] for i in range(len(nums)+1)]
+    # print(freq)
+    for i in nums:
+        count[i] = 1 + count.get(i,0)
+    # print(count)
+    for n , c in count.items():
+        freq[c].append(n)
+    # print(freq)
+    res = []
+    for i in range(len(freq)-1,0,-1):
+        for n in freq[i]:
+            res.append(n)
+            # print(res)
+            if len(res) == k:
+                return res
+nums = [1,1,1,2,2,3]
+k = 2
+print(topKFrequent(nums, k))
