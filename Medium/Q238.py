4abcb1a2826f4930fdd18dc1cdda9b418acc5944
harsh28060 <harshpanchigar@gmail.com>
2026-05-21 22:55:08 +0530

    initial commit

--- Medium/Q238.py
+++ Medium/Q238.py
@@ -1,13 +1,16 @@
-nums = [1,2,3,4]
+nums = [2,3,4,5]
 
 res = [1] * len(nums)
 prefix = 1
 for i in range(len(nums)):
     res[i] = prefix
     prefix *= nums[i]
+
+
 print(res)
 postfix = 1
 for i in range(len(nums)-1,-1,-1):
     res[i] *= postfix
     postfix *= nums[i]
+
 print(res)
---
+++ Medium/tempCodeRunnerFile.py
@@ -0,0 +1,3 @@
+x = 1
+for i in range(len(nums)-1,-1,-1):
+    res[i] *=
